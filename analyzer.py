80d2585f64b4ac2913913b04fb2402d39a4e5a78
Afia Jahan K <k.afiajahan@gmail.com>
2026-04-26 22:07:50 +0530

    Add log analyzer to summarize log file entries

---
+++ analyzer.py
@@ -0,0 +1,23 @@
+log_file = "logs.txt"
+
+info = 0
+error = 0
+warning = 0
+
+with open(log_file, "r") as file:
+    for line in file:
+        if "INFO" in line:
+            info += 1
+        elif "ERROR" in line:
+            error += 1
+        elif "WARNING" in line:
+            warning += 1
+
+with open("report.txt", "w") as report:
+    report.write("Log Summary Report\n")
+    report.write("-------------------\n")
+    report.write(f"INFO: {info}\n")
+    report.write(f"ERROR: {error}\n")
+    report.write(f"WARNING: {warning}\n")
+
+print("Report Generated Successfully!")
